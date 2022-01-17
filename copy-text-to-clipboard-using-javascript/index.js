ce8aacf2e37f2dc243c137de07fa023cd2e97b6e
Dev-Raaz <singhdebraj51@gmail.com>
2022-01-17 23:30:14 +0200

    Updated some animations

--- copy-text-to-clipboard-using-javascript/index.js
+++ copy-text-to-clipboard-using-javascript/index.js
@@ -2,13 +2,16 @@
 const copyBtn = document.querySelector('button')
 
 copyBtn.addEventListener('click', ()=>{
-    copyBtn.classList.add('show-msg')
+    
 
     //Selects the text field which is the first child of the .filed div
     const textField = document.querySelector('.field .text')
     
-    //Copies the value of the text inside the p tag to the clipboard
-    navigator.clipboard.writeText(textField.value)
+    if(textField.value != ''){
+        copyBtn.classList.add('show-msg')
+        //Copies the value of the text inside the p tag to the clipboard
+        navigator.clipboard.writeText(textField.value)
+    }
 })
 copyBtn.addEventListener('mouseleave', ()=>{
     copyBtn.classList.remove('show-msg')
