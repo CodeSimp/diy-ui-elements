a5b1d499b230d394cbb4410eb7c2d596be140f66
Dev-Raaz <singhdebraj51@gmail.com>
2022-01-18 17:24:50 +0200

    How to toggle a searchbar using keypress

--- copy-text-to-clipboard-using-javascript/index.js
+++ copy-text-to-clipboard-using-javascript/index.js
@@ -1,18 +1,21 @@
-//Selects the button
+//Selects the copy button
 const copyBtn = document.querySelector('button')
 
+//Listening to the button click
 copyBtn.addEventListener('click', ()=>{
     
-
     //Selects the text field which is the first child of the .filed div
     const textField = document.querySelector('.field .text')
     
+    //Copies only when there is some text to copy
     if(textField.value != ''){
         copyBtn.classList.add('show-msg')
         //Copies the value of the text inside the p tag to the clipboard
         navigator.clipboard.writeText(textField.value)
     }
 })
+
+//Removes the tooltip when mouse leaves the copy btn
 copyBtn.addEventListener('mouseleave', ()=>{
     copyBtn.classList.remove('show-msg')
 })
---
+++ toggle-search-bar-on-keypress/index.js
@@ -0,0 +1,43 @@
+const h3s = document.querySelectorAll('h3')
+const frequentlySearched = document.querySelector('.frequently-searched')
+const recentSearches = document.querySelectorAll('.recent-search')
+const searchField = document.querySelector('input')
+const openSearch = document.querySelector('button.open-search')
+const closeSearch = document.querySelector('button.close-search')
+
+
+const openSearchBar = ()=>{
+    recentSearches.forEach(obj => obj.classList.remove('hide'))
+    h3s.forEach(obj => obj.classList.remove('hide'))
+    frequentlySearched.classList.remove('hide')
+    closeSearch.classList.remove('hide')
+    openSearch.classList.add('hide')
+    searchField.focus()
+}
+const closeSearchBar = ()=>{
+    recentSearches.forEach(obj => obj.classList.add('hide'))
+    h3s.forEach(obj => obj.classList.add('hide'))
+    frequentlySearched.classList.add('hide')
+    closeSearch.classList.add('hide')
+    openSearch.classList.remove('hide') 
+    searchField.blur()
+}
+
+//The searchbar is closed initially
+closeSearchBar()
+
+openSearch.addEventListener('click', ()=>{
+    openSearchBar()  
+})
+closeSearch.addEventListener('click', ()=>{
+    closeSearchBar()  
+})
+
+document.addEventListener('keydown', (e)=>{
+    console.log(e)
+    if(e.key === 'Control')
+        openSearchBar()
+    else if(e.key === 'Escape')
+        closeSearchBar()
+})
+
