dff8366ac3ffe59415f8d6677348ce00cc390bed
Dev-Raaz <singhdebraj51@gmail.com>
2022-03-12 09:30:45 +0200

    Added Jokes

---
+++ haha-hehe/index.js
@@ -0,0 +1,23 @@
+
+const getJoke = async() => {
+    //Getting the joke container
+    const joke = document.querySelector('#joke p')
+    const getLoader = document.querySelector('#joke .spinner')
+
+    getLoader.classList.toggle('hide')
+    joke.classList.toggle('hide')
+    const res = await axios.get('https://icanhazdadjoke.com/',{
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    
+    getLoader.classList.toggle('hide')
+    joke.classList.toggle('hide')
+    joke.innerHTML = res.data.joke
+}
+
+document.querySelector('#get-joke')
+.addEventListener('click', ()=>{
+    getJoke()
+})
